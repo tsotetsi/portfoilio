dd6db0f5c8a06247568645f6e74c84e15c4c309d
Oslinjohn20 <ossiekcjohnson@gmail.com>
2020-07-07 14:58:35 +0200

    Updates

---
+++ precache-manifest.d127e9d84692cc513eda7977cb13f32a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53231f420c9d5c030e6d26253b9eb678",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8d536a794ca97c2929f7",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "3c2ebde29329ff4b4e43",
+    "url": "/portfolio/static/css/main.da079710.chunk.css"
+  },
+  {
+    "revision": "8d536a794ca97c2929f7",
+    "url": "/portfolio/static/js/2.a1b12462.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.a1b12462.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c2ebde29329ff4b4e43",
+    "url": "/portfolio/static/js/main.6a9790b9.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "f7303d665c1c61a6b3f1079575e40848",
+    "url": "/portfolio/static/media/contact-keeper.f7303d66.png"
+  },
+  {
+    "revision": "71f35d476a8943d2f3837b597e20fd83",
+    "url": "/portfolio/static/media/github-finder.71f35d47.png"
+  },
+  {
+    "revision": "9dab37dae31a25fb6611824dce3aabb2",
+    "url": "/portfolio/static/media/it-logger.9dab37da.png"
+  },
+  {
+    "revision": "199d57e022ee33aba57c560f001876e3",
+    "url": "/portfolio/static/media/profile.199d57e0.jpeg"
+  }
+]);
